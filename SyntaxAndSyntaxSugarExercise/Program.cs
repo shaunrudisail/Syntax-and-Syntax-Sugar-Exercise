4b762f8862265b27dda45cd69e80904d49e092ea
Shaun Rudisail <sprudisail@gmail.com>
2025-06-15 00:43:37 -0400

    Completed Syntax Sugar Exercise.

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer = 4;
+            var response = answer < 9 ? $"{answer} is less than 9" : $"{answer} is greater than or equal to 9";
+            Console.WriteLine(response);
         }
     }
 }
